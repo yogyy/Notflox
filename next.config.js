4676025a4d8b8f299874dea1a9259fa05cb468ee
yogyy <yogimr261@gmail.com>
2023-04-02 23:57:09 +0700

    image config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['image.tmdb.org'],
+    domains: [
+      'image.tmdb.org',
+      'avatars.githubusercontent.com',
+      'lh3.googleusercontent.com',
+    ],
   },
 };
 
