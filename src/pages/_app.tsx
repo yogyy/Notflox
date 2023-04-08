2b808e219d95fb96bd8de6e6b4b39e7c21bebf81
yogyy <yogimr261@gmail.com>
2023-04-08 23:57:56 +0700

    nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,14 @@ import { getSession, SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
 
+import '@/styles/nprogress.css';
+import { Router } from 'next/router';
+import nProgress from 'nprogress';
+
+Router.events.on('routeChangeStart', nProgress.start);
+Router.events.on('routeChangeError', nProgress.done);
+Router.events.on('routeChangeComplete', nProgress.done);
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
