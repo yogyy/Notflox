fd21d7ab1b362ba898caad6b2d71184e939acf1b
yogyy <yogimr261@gmail.com>
2023-04-10 23:48:32 +0700

    edit navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -12,6 +12,7 @@ import NavbarItem from './NavbarItem';
 import MobileMenu from './MobileMenu';
 import Image from 'next/image';
 import Search from './netflix1/Search';
+import Drawer from './netflix1/Drawer';
 
 const TOP_OFFSET = 66;
 
@@ -24,7 +25,6 @@ const Navbar = () => {
 
   React.useEffect(() => {
     const handleScroll = () => {
-      // console.log(window.scrollY);
       if (window.scrollY >= TOP_OFFSET) {
         setShowBackground(true);
       } else {
@@ -50,12 +50,15 @@ const Navbar = () => {
   return (
     <nav className="w-full fixed z-20">
       <div
-        className={`px-4 md:px-16 py-2 md:py-6 flex flex-row items-center transition duration-500 ${
+        className={`px-4 py-2 md:py-6 flex flex-row items-center transition duration-500 ${
           showBackground ? 'bg-zinc-900/90' : ''
         }`}
       >
-        <Netflix className="h-4 lg:h-7" />
-        <NavbarItem />
+        <div className="flex items-center">
+          <Drawer />
+          <Netflix className="h-4 lg:h-7" />
+        </div>
+        <NavbarItem className="flex-row ml-8 gap-7 hidden lg:flex" />
         {/* <div
           onClick={toggleMobileMenu}
           className="lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative"
@@ -69,13 +72,15 @@ const Navbar = () => {
           <MobileMenu visible={showMobileMenu} />
         </div> */}
         <div className="flex flex-row ml-auto gap-3 md:gap-5 items-center">
-          <Search />
+          <div className="hidden md:block">
+            <Search />
+          </div>
           <button className="text-gray-200 hover:text-gray-300 cursor-pointer transition ">
             <BellIcon className="w-6 m-1" />
           </button>
           <button
             onClick={toggleAccountMenu}
-            className="flex flex-row items-center gap-2 cursor-pointer relative focus:outline-blue-500 pr-1"
+            className="flex flex-row items-center gap-2 cursor-pointer relative focus:outline-blue-500 p-1"
           >
             <div className="w-6 h-6 lg:w-10 lg:h-10 rounded-md overflow-hidden">
               <Image
