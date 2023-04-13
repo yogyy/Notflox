8fe9422bdf3d47cf00b8609f6667be48876766bb
yogyy <yogimr261@gmail.com>
2023-04-13 23:54:55 +0700

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,10 +4,10 @@ export default function middleware(request: {
   url: string | URL | undefined;
   nextUrl: { pathname: string };
 }) {
-  if (request.nextUrl.pathname.startsWith('/tv/')) {
+  if (request.nextUrl.pathname == '/tv') {
     return NextResponse.redirect(new URL('/series', request.url));
   }
-  if (request.nextUrl.pathname === '/movie/') {
+  if (request.nextUrl.pathname == '/movie') {
     return NextResponse.redirect(new URL('/movies', request.url));
   }
   return NextResponse.next();
