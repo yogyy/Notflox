4c95e990bd1f3e0ea3cc64f9a699298556e25c91
yogyy <yogimr261@gmail.com>
2023-04-19 23:57:38 +0700

    add loading

---
+++ src/components/loading.tsx
@@ -0,0 +1,11 @@
+import styles from '@/styles/Loader.module.css';
+
+const loading = () => {
+  return (
+    <div className="">
+      <div className={styles.loadingcoy}></div>
+    </div>
+  );
+};
+
+export default loading;
