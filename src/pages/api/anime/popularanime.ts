6fa633d48ca4a90ec8f35f546785734d28a5bb65
yogyy <yogimr261@gmail.com>
2023-04-20 23:28:35 +0700

    anime api

---
+++ src/pages/api/anime/popularanime.ts
@@ -0,0 +1,26 @@
+import { BASE_URL, API_KEY } from '@/utils/request';
+import axios from 'axios';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const currentDate = new Date();
+  const year = currentDate.getFullYear();
+  const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+  const day = currentDate.getDate().toString().padStart(2, '0');
+
+  const today = `${year}-${month}-${day}`;
+  if (req.method !== 'GET') {
+    return res.status(405).end();
+  }
+  try {
+    const response = await axios.get(
+      `${BASE_URL}/discover/tv?api_key=${API_KEY}&sort_by=popularity.desc&air_date.gte=${today}&first_air_date_year=2023&with_genres=16&with_original_language=ja`
+    );
+    res.status(200).json(response.data.results);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+}
+
+export default handler;
---
+++ src/pages/api/anime/recentanime.ts
@@ -0,0 +1,35 @@
+import { BASE_URL, API_KEY } from '@/utils/request';
+import axios from 'axios';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'GET') {
+    return res.status(405).end();
+  }
+  try {
+    const pages = 1;
+    const baseUrl = `${BASE_URL}/tv/on_the_air`;
+    const apiKey = API_KEY;
+    const timezone = 'Japan/Tokyo';
+    const genre = 16;
+    const language = 'ja';
+    const keywords = 'anime';
+    const totalPages = 4;
+    const requests = [];
+
+    for (let page = 1; page <= totalPages; page++) {
+      const url = `${baseUrl}?api_key=${apiKey}&page=${page}&timezone=${timezone}&with_genres=${genre}&with_original_language=${language}&with_keywords=${keywords}`;
+      requests.push(axios.get(url));
+    }
+    const responses = await Promise.all(requests);
+    const data = responses.map(response => response.data.results);
+    const combine = data.flat();
+
+    res.status(200).json(combine);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+}
+
+export default handler;
