209e8983e4aea28fec25f73fa84fad8f14488b99
yogyy <yogimr261@gmail.com>
2023-04-24 20:52:37 +0700

    error input

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,12 +1,9 @@
-import MyCombobox from '@/components/input';
 import React from 'react';
 
 const Test = () => {
   return (
     <div className="w-full flex justify-center">
-      <div className="">
-        <MyCombobox />
-      </div>
+      <div className=""></div>
       <div className="h-20 w-20 skeleton"></div>
     </div>
   );
