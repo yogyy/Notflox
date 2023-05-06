933938e2c23dce46a224850848e8429ff9a3a35c
yogyy <yogimr261@gmail.com>
2023-05-06 08:24:40 +0700

    zustand

--- atoms/modalAtom.ts
+++ atoms/modalAtom.ts
@@ -1,5 +1,7 @@
 import { atom } from 'recoil';
 import { Movie } from '../typing';
+import { create } from 'zustand';
+import { ImageProps } from 'next/image';
 
 export const modalState = atom({
   key: 'modalState',
@@ -10,3 +12,29 @@ export const movieState = atom<Movie | null>({
   key: 'movieState',
   default: null,
 });
+
+const images = [
+  '/images/default-blue.png',
+  '/images/default-red.png',
+  '/images/default-slate.png',
+  '/images/default-green.png',
+];
+
+interface AppState {
+  showModal: boolean;
+  currentMovie: Movie | null;
+  setCurrentMovie: (movie: Movie | null) => void;
+  setShowModal: (isOpen: boolean) => void;
+}
+
+export const useAppState = create<AppState>(set => ({
+  showModal: false,
+  currentMovie: null,
+  setCurrentMovie: (movie: Movie | null) => set({ currentMovie: movie }),
+  setShowModal: (isOpen: boolean) => set({ showModal: isOpen }),
+}));
+
+export const useProfileStore = create(set => ({
+  imaged: '/images/default-red.png',
+  setNonloginProfile: (newImage: ImageProps) => set({ imaged: newImage }),
+}));
