108a5590138f34194dc88da34c45217f7de2a437
yogyy <yogimr261@gmail.com>
2023-05-09 04:00:29 +0700

    add umami Analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          defer
+          src="https://analytics.umami.is/script.js"
+          data-website-id={process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID || ''}
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
