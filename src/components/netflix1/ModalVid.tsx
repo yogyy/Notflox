783f709f810fab1b95678aa3848a5c84334f4979
yogyy <yogimr261@gmail.com>
2023-05-20 08:32:21 +0700

    search with modal + new modal

--- src/components/netflix1/ModalVid.tsx
+++ src/components/netflix1/ModalVid.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import Modal from '@mui/material/Modal';
 import { XMarkIcon } from '@heroicons/react/24/outline';
 import Image from 'next/image';
 import { Element, Genre, ProductionCompany } from '../../../typing';
@@ -7,13 +6,12 @@ import { API_KEY, BASE_URL } from '@/utils/request';
 import ReactPlayer from 'react-player/youtube';
 import axios from 'axios';
 import { useQuery } from '@tanstack/react-query';
-import LoaderBlock from '../loader/loaderblock';
 import { modalState, movieState } from '../../../atoms/jotaiAtoms';
 import { useAtom } from 'jotai';
 import { tanggal } from '@/lib/getDate';
 import clsx from 'clsx';
 import { toast } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
+import { Dialog, Transition } from '@headlessui/react';
 
 interface modalProps {
   showDetail?: boolean;
@@ -22,7 +20,6 @@ interface modalProps {
 function ModalVid({ showDetail }: modalProps) {
   const [showModal, setShowModal] = useAtom(modalState);
   const [movie, setMovie] = useAtom(movieState);
-
   const [trailer, setTrailer] = React.useState([]);
   const [genres, setGenres] = React.useState<Genre[]>([]);
   const [networks, setNetworks] = React.useState<ProductionCompany[]>([]);
@@ -33,7 +30,7 @@ function ModalVid({ showDetail }: modalProps) {
     setMovie(null);
   };
   const [videoError, setVideoError] = React.useState(false);
-  const { data, isError, isFetching } = useQuery(
+  const { data } = useQuery(
     ['movie', movie?.id],
     async () => {
       const response = await axios.get(
@@ -52,7 +49,7 @@ function ModalVid({ showDetail }: modalProps) {
   React.useEffect(() => {
     if (data?.videos) {
       const index = data.videos.results.findIndex(
-        (element: Element) => element.type === 'Trailer' || 'Opening Credits'
+        (element: Element) => element.type === 'Trailer'
       );
       setTrailer(data.videos?.results[index]?.key);
     }
@@ -64,144 +61,157 @@ function ModalVid({ showDetail }: modalProps) {
     }
   }, [data]);
 
-  if (isError)
-    return (
-      <div className="fixed inset-0 bg-[#121212]/50 backdrop-blur-sm z-40">
-        <LoaderBlock />
-      </div>
-    );
-
   return (
-    <Modal
-      open={showModal}
-      onClose={handleClose}
-      className={clsx(
-        showDetail === false ? '!top-32' : '!top-7 left-0 right-0',
-        'fixed z-50 mx-auto w-full max-w-5xl overflow-hidden overflow-y-scroll rounded-md scrollbar-hide px-2'
-      )}
-    >
-      <>
-        {trailer && (
-          <button
-            className="modalButton xl:hidden absolute right-3 top-3 !z-20 h-6 w-6 xl:h-9 xl:w-9 border-none bg-[#181818] hover:bg-[#181818]"
-            onClick={handleClose}
+    <div className="">
+      <Transition appear show={showModal} as={React.Fragment}>
+        <Dialog as="div" className="relative z-30" onClose={handleClose}>
+          <Transition.Child
+            as={React.Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
           >
-            <XMarkIcon className="w-4 h-4" />
-          </button>
-        )}
-        <div className="relative pt-[56.25%]">
-          {trailer && !videoError ? (
-            <ReactPlayer
-              url={`https://www.youtube.com/watch?v=${trailer}`}
-              width="100%"
-              height="100%"
-              style={{
-                position: 'absolute',
-                top: '0',
-                left: '0',
-              }}
-              controls
-              playing={true}
-              muted={muted}
-              volume={0.3}
-              onError={() => {
-                setVideoError(true);
-                toast.error('Trailer not available', {
-                  position: 'top-right',
-                  autoClose: 3000,
-                  hideProgressBar: true,
-                  closeOnClick: true,
-                  pauseOnHover: false,
-                  draggable: true,
-                  progress: undefined,
-                  theme: 'dark',
-                });
-              }}
-            />
-          ) : (
-            <Image
-              src={`https://image.tmdb.org/t/p/original/${
-                movie?.backdrop_path || movie?.poster_path
-              }`}
-              className="object-cover rounded-t-sm md:rounded-t"
-              fill
-              sizes="100%"
-              alt={`Thumbnail ${movie?.name || movie?.title}`}
-              draggable={false}
-            />
-          )}
-        </div>
-        {showDetail === true && (
-          <div className="flex space-x-16 rounded-b-md bg-[#121212]/95 px-10 py-8">
-            <div className="space-y-6 text-lg">
-              <h1 className="text-xl font-bold">
-                {movie?.title || movie?.name}
-              </h1>
-              <div className="flex items-center space-x-2 text-sm ">
-                <p className="text-sm font-semibold text-green-400">
-                  {movie &&
-                    `${(movie.vote_average * 10)
-                      .toString()
-                      .slice(0, 2)}% Match`}
-                </p>
-                <p className="font-light">
-                  {movie?.media_type === 'movie'
-                    ? tanggal(movie?.release_date)
-                    : tanggal(movie?.first_air_date)}
-                </p>
-                <div className="flex h-4 items-center justify-center rounded border border-white/40 px-1.5 text-xs">
-                  HD
-                </div>
-              </div>
-              <div className="flex flex-col-reverse justify-between font-light gap-x-10 gap-y-4">
-                <p className="w-5/6 text-gray-300">
-                  {movie?.overview}{' '}
-                  {!movie?.overview && (
-                    <>
-                      <span className="text-red-500">
-                        overview not avaiable.
-                      </span>{' '}
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Cumque at illo vero eius doloremque, ut magnam nobis
-                      minima, officia odit quia?
-                    </>
-                  )}
-                </p>
-                <div className="flex flex-col space-y-3 text-sm">
-                  <div>
-                    <p>
-                      Genres :&nbsp;
-                      <span className="font-semibold">
-                        {genres.map(genre => genre.name).join(', ')}
-                      </span>
-                    </p>
-                  </div>
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+          <div
+            className={clsx(
+              'fixed inset-0 pt-20 overflow-x-hidden overflow-y-scroll rounded-md scrollbar-hide',
+              !showDetail && ''
+            )}
+          >
+            <div className="flex min-h-full items-start justify-center p-4 text-center">
+              <Transition.Child
+                as={React.Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="max-w-5xl w-[64rem] transform rounded-2xl text-left align-middle shadow-xl transition-all">
+                  <div className="text-lg font-medium leading-6 text-gray-300">
+                    <div className="relative max-w-5xl w-full h-auto aspect-video">
+                      {trailer && !videoError ? (
+                        <ReactPlayer
+                          url={`https://www.youtube.com/watch?v=${trailer}`}
+                          width="100%"
+                          height="100%"
+                          controls
+                          style={{ background: '#121212' }}
+                          playing={true}
+                          muted={muted}
+                          volume={0.3}
+                          onError={() => {
+                            setVideoError(true);
+                            toast.error('Trailer not available', {
+                              position: 'top-right',
+                              autoClose: 3000,
+                              hideProgressBar: true,
+                              closeOnClick: true,
+                              pauseOnHover: false,
+                              draggable: true,
+                              progress: undefined,
+                              theme: 'dark',
+                            });
+                          }}
+                        />
+                      ) : (
+                        <Image
+                          src={`https://image.tmdb.org/t/p/original/${
+                            movie?.backdrop_path || movie?.poster_path
+                          }`}
+                          className="object-cover rounded-t-sm md:rounded-t"
+                          fill
+                          sizes="100%"
+                          alt={`Thumbnail ${movie?.name || movie?.title}`}
+                          draggable={false}
+                        />
+                      )}
+                    </div>
+                    {showDetail === true && (
+                      <div className="flex space-x-16 rounded-b-md bg-[#121212]/95 px-10 py-8">
+                        <div className="space-y-6 text-lg">
+                          <h1 className="text-xl font-bold">
+                            {movie?.title || movie?.name}
+                          </h1>
+                          <div className="flex items-center space-x-2 text-sm ">
+                            <p className="text-sm font-semibold text-green-400">
+                              {movie &&
+                                `${(movie.vote_average * 10)
+                                  .toString()
+                                  .slice(0, 2)}% Match`}
+                            </p>
+                            <p className="font-light">
+                              {movie?.media_type === 'movie'
+                                ? tanggal(movie?.release_date)
+                                : tanggal(movie?.first_air_date)}
+                            </p>
+                            <div className="flex h-4 items-center justify-center rounded border border-white/40 px-1.5 text-xs">
+                              HD
+                            </div>
+                          </div>
+                          <div className="flex flex-col-reverse justify-between font-light gap-x-10 gap-y-4">
+                            <p className="w-5/6 text-gray-300">
+                              {movie?.overview}{' '}
+                              {!movie?.overview && (
+                                <>
+                                  <span className="text-red-500">
+                                    overview not avaiable.
+                                  </span>{' '}
+                                  Lorem ipsum dolor sit amet consectetur
+                                  adipisicing elit. Cumque at illo vero eius
+                                  doloremque, ut magnam nobis minima, officia
+                                  odit quia?
+                                </>
+                              )}
+                            </p>
+                            <div className="flex flex-col space-y-3 text-sm">
+                              <div>
+                                <p>
+                                  Genres :&nbsp;
+                                  <span className="font-semibold">
+                                    {genres.map(genre => genre.name).join(', ')}
+                                  </span>
+                                </p>
+                              </div>
 
-                  <div>
-                    <p>
-                      Original language:&nbsp;
-                      <span className="font-semibold">
-                        {movie?.original_language.toUpperCase()}
-                      </span>
-                    </p>
-                  </div>
-                  <div className="flex w-full">
-                    <p>
-                      Sudio :&nbsp;
-                      <span className="font-semibold">
-                        {networks.map(network => network.name).join(', ')}
-                      </span>
-                    </p>
-                  </div>
+                              <div>
+                                <p>
+                                  Original language:&nbsp;
+                                  <span className="font-semibold">
+                                    {movie?.original_language.toUpperCase()}
+                                  </span>
+                                </p>
+                              </div>
+                              <div className="flex w-full">
+                                <p>
+                                  Sudio :&nbsp;
+                                  <span className="font-semibold">
+                                    {networks
+                                      .map(network => network.name)
+                                      .join(', ')}
+                                  </span>
+                                </p>
+                              </div>
 
-                  <div className=""></div>
-                </div>
-              </div>
+                              <div className=""></div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    )}
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
             </div>
           </div>
-        )}
-      </>
-    </Modal>
+        </Dialog>
+      </Transition>
+    </div>
   );
 }
 
--- src/components/netflix1/Search.tsx
+++ src/components/netflix1/Search.tsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 import * as React from 'react';
 import { Movie } from '../../../typing';
 import clsx from 'clsx';
-import { Transition } from '@headlessui/react';
+import { Dialog, Transition } from '@headlessui/react';
 import Link from 'next/link';
 import { useSession } from 'next-auth/react';
 import { toast, ToastContainer } from 'react-toastify';
@@ -17,14 +17,19 @@ const Search = () => {
   const [debouncedQuery, setDebouncedQuery] = React.useState('');
   const [showResults, setShowResults] = React.useState(false);
   const { data: session } = useSession();
+  const [open, setOpen] = React.useState(false);
+  function closeModal() {
+    setOpen(false);
+    setShowInput(false);
+  }
 
+  function openModal() {
+    setOpen(true);
+    setShowInput(true);
+  }
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setQuery(e.target.value);
     setShowResults(e.target.value.length >= 3);
-  };
-
-  const handleSubmit = (e: { preventDefault: () => void }) => {
-    e.preventDefault();
   };
 
   const searchMovies = React.useCallback(async (query: string) => {
@@ -69,32 +74,9 @@ const Search = () => {
     });
 
   return (
-    <div className="flex items-end">
-      <Transition
-        show={showInput}
-        enter="transition-opacity duration-700"
-        enterFrom="opacity-0"
-        enterTo="opacity-100"
-        leave="transition-opacity duration-350"
-        leaveFrom="opacity-100"
-        leaveTo="opacity-0"
-      >
-        <input
-          type="search"
-          placeholder="Search movie.."
-          value={query}
-          onChange={handleInputChange}
-          onClick={() => setShowResults(true)}
-          className={clsx(
-            showInput ? ' bg-white/5 block' : 'bg-transparent',
-            ' px-2 py-1 transition-all  rounded focus:outline-none'
-          )}
-        />
-      </Transition>
+    <div className="relative flex items-end">
       <button
-        onClick={() => {
-          session ? setShowInput(!showInput) : error();
-        }}
+        onClick={openModal}
         title="Search Movie"
         className="ml-4 text-gray-200 transition rounded-full cursor-pointer hover:text-gray-300 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-600"
       >
@@ -105,62 +87,127 @@ const Search = () => {
         />
       </button>
       <ToastContainer limit={1} />
-      <div className="absolute top-10">
-        <Transition
-          show={showInput}
-          enter="transition-opacity duration-700"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="transition-opacity duration-350"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
+      <div className="">
+        <Dialog
+          as="div"
+          open={open}
+          className="relative z-30"
+          onClose={closeModal}
         >
-          {showInput && (
-            <div className={'max-h-40 overflow-y-auto transition-transform'}>
-              {query.length >= 3 && (
-                <ul className="flex flex-col bg-zinc-900/80">
-                  <div className=""></div>
-                  {searchResults.map(result => (
-                    <li
-                      className="w-[245px] py-2 hover:cursor-pointer"
-                      key={result.id}
-                      onClick={() => {
-                        setSearchResults([]);
-                      }}
-                    >
-                      <Link
-                        href={{
-                          pathname: `/${
-                            result.media_type == 'movie' ? 'movie' : 'tv'
-                          }/${result.id}`,
-                          query: { title: `${result.name || result.title}` },
-                        }}
-                        // href="#"
-
-                        className="flex justify-between mx-2"
+          <div className="fixed inset-0  bg-black bg-opacity-60" />
+          <div className="fixed inset-0 overflow-hidden">
+            <div className="flex min-h-full pt-10 items-start justify-center p-4 text-center">
+              <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded bg-[#121212] text-left align-middle shadow-xl transition-all">
+                <Dialog.Title
+                  as="h2"
+                  className="text-lg font-medium leading-6 text-gray-300"
+                >
+                  <Transition
+                    show={open}
+                    as={React.Fragment}
+                    enter="transition-opacity duration-75"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="transition-opacity duration-150"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                  >
+                    <div className="flex items-center border-b p-3">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        className="w-5 h-5"
                       >
-                        <span>
-                          {result.title || result.name}
-                          <span className="ml-1.5">
-                            {result.release_date && (
-                              <>({result.release_date.slice(0, 4)})</>
-                            )}
-                            {result.first_air_date && (
-                              <>({result.first_air_date.slice(0, 4)})</>
-                            )}
-                          </span>
-                        </span>
-                        <span className="px-1 bg-gray-800 rounded-md h-fit">
-                          {result.media_type}
-                        </span>
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              )}
+                        <path
+                          fillRule="evenodd"
+                          d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+
+                      <input
+                        type="search"
+                        placeholder="Search movie.."
+                        value={query}
+                        onChange={handleInputChange}
+                        className={clsx(
+                          'bg-transparent',
+                          'px-3 py-1 transition-all w-full rounded outline-none'
+                        )}
+                      />
+                    </div>
+                  </Transition>
+                </Dialog.Title>
+                <Transition
+                  show={open}
+                  as={React.Fragment}
+                  enter="transition-opacity duration-200"
+                  enterFrom="opacity-0"
+                  enterTo="opacity-100"
+                  leave="transition-opacity duration-150"
+                  leaveFrom="opacity-100"
+                  leaveTo="opacity-0"
+                >
+                  <div className="">
+                    <div
+                      className={'h-full overflow-y-auto transition-transform'}
+                    >
+                      {query.length >= 3 && (
+                        <ul className="flex flex-col ">
+                          {searchResults.map(result => (
+                            <li
+                              key={result.id}
+                              onClick={() => {
+                                setSearchResults([]);
+                              }}
+                            >
+                              <Link
+                                href={{
+                                  pathname: `/${
+                                    result.media_type == 'movie'
+                                      ? 'movie'
+                                      : 'tv'
+                                  }/${result.id}`,
+                                  query: {
+                                    title: `${result.name || result.title}`,
+                                  },
+                                }}
+                                // href="#"
+                                className={clsx(
+                                  'w-full px-2 py-2 flex justify-between outline-none',
+                                  'hover:bg-black/50 transition-colors duration-200',
+                                  'focus-within:bg-current/hover:bg-black/50 group'
+                                )}
+                              >
+                                <p>
+                                  <span className="group-hover:text-red-600">
+                                    {result.title || result.name}
+                                  </span>
+                                  <span className="ml-1.5">
+                                    {result.release_date && (
+                                      <>({result.release_date.slice(0, 4)})</>
+                                    )}
+                                    {result.first_air_date && (
+                                      <>({result.first_air_date.slice(0, 4)})</>
+                                    )}
+                                  </span>
+                                </p>
+                                <span className="px-1 h-fit">
+                                  {result.media_type}
+                                </span>
+                              </Link>
+                            </li>
+                          ))}
+                        </ul>
+                      )}
+                    </div>
+                  </div>
+                </Transition>
+              </Dialog.Panel>
             </div>
-          )}
-        </Transition>
+          </div>
+        </Dialog>
       </div>
     </div>
   );
