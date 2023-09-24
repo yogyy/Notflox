3d2830432584ca9f77af7f59f0ac99d0ed5ff58d
yogyy <yogimr261@gmail.com>
2023-09-24 20:49:09 +0700

    chore: fix height 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,8 +3,8 @@ import RootLayout from '@/components/layouts/layout';
 export default function Custom404() {
   return (
     <RootLayout title={'not found'}>
-      <div className="flex h-screen w-screen items-center justify-center font-mono">
-        <h1 className="text-3xl mr-4">404</h1>
+      <div className="flex h-[calc(100vh_-_133px)] w-screen items-center justify-center font-mono">
+        <h1 className="mr-4 text-3xl text-primary">404</h1>
         <div>
           <h2>This page could not be found </h2>
         </div>
