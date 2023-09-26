20dc20da06c920fb949e28f15d9edeaddc6b271f
yogyy <yogimr261@gmail.com>
2023-09-26 23:21:02 +0700

    ✨feat: change drawer MUI to RadixUI(shadcn)

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,9 +3,17 @@ import AccountMenu from './AccountMenu';
 import { Netflix } from './icons';
 import NavbarItem from './NavbarItem';
 import Search from './netflix1/Search';
-import Drawer from './netflix1/Drawer';
 import Notifications from './notification';
 import Link from 'next/link';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/UI/sheet';
+import { Bars3BottomLeftIcon } from '@heroicons/react/24/outline';
 
 const TOP_OFFSET = 100;
 
@@ -33,16 +41,28 @@ const Navbar = () => {
       <div
         className={`h-16 flex flex-row items-center transition duration-500 px-5 ${
           showBackground ? 'bg-zinc-900/90' : ''
-        }`}
-      >
+        }`}>
         <div className="flex items-center justify-center">
-          <Drawer />
+          <Sheet>
+            <SheetTrigger className="block sm:hidden">
+              <Bars3BottomLeftIcon className="w-5 text-red-500" />
+            </SheetTrigger>
+            <SheetContent side="left" className="bg-zinc-900/90">
+              <SheetHeader>
+                <SheetTitle className="mb-4">
+                  <Netflix className="h-4" />
+                </SheetTitle>
+                <SheetDescription>
+                  <NavbarItem className="flex flex-col items-start gap-3" />
+                </SheetDescription>
+              </SheetHeader>
+            </SheetContent>
+          </Sheet>
           <Link
             href="/"
             className="flex items-center justify-center focus:outline-none focus-visible:ring-2 focus-visible:ring-red-600 focus:py-1"
-            title="beranda Notflox"
-          >
-            <Netflix className="h-4 lg:h-7 mt-1 w-fit" />
+            title="beranda Notflox">
+            <Netflix className="h-4 mt-1 lg:h-7 w-fit" />
           </Link>
           <NavbarItem className="flex-row hidden ml-4 sm:gap-3 md:gap-7 sm:flex" />
         </div>
