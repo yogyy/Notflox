08ca1b69cbad0bb4528cd5144a862e003a859f0d
yogyy <yogimr261@gmail.com>
2023-09-30 23:58:59 +0700

    chore: update jotai atom & delete zustand store

--- atoms/jotaiAtoms.ts
+++ atoms/jotaiAtoms.ts
@@ -4,3 +4,16 @@ import { Movie } from '../typing';
 export const modalState = atom<boolean>(false);
 
 export const movieState = atom<Movie | null>(null);
+
+export const changeModalState = atom(null, (get, set, newState: boolean) => {
+  set(modalState, newState);
+});
+
+export const changeMovieState = atom(
+  null,
+  (get, set, newMovie: Movie | null) => {
+    set(movieState, newMovie);
+  }
+);
+
+export const nonUser = atom('/images/default-red.png');
