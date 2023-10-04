63b1223cef9a950ecb00926314223c6328415af5
yogyy <yogimr261@gmail.com>
2023-10-04 17:25:24 +0700

    chore: update footer on mobile

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -6,18 +6,19 @@ const date = new Date().getFullYear();
 export default function Footer() {
   return (
     <footer className="bg-ireng transition-colors">
-      <hr className="my-6 sm:mx-auto border-gray-700" />
-      <div className="p-4 max-w-[1300px] mx-auto relative">
+      <hr className="md:mb-6 pt-4 sm:mx-auto border-gray-700" />
+      <div className="p-2.5 md:p-4 max-w-7xl mx-auto relative">
         <div className="flex flex-col justify-center mb-4 sm:flex-row sm:justify-between">
           <Netflix />
         </div>
-        <div className="flex flex-col justify-center gap-6 md:flex-row md:justify-between text-gray-400">
-          <span className="flex gap-3 text-sm font-semibold sm:text-center items-center">
+        <div className="flex gap-6 flex-row justify-between text-gray-400 items-center">
+          <span className="flex gap-3 text-sm font-semibold">
             © {date} yogyy.
           </span>
           <Link
-            className="hover:text-gray-600 dark:hover:text-gray-200"
+            className="hover:text-gray-600 dark:hover:text-gray-200 font-semibold"
             href="https://github.com/yogyy/Netflix-Movie-App"
+            title="source code"
             target="_blank">
             Github
           </Link>
