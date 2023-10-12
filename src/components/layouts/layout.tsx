a56a78761a5e820045acff11d3868ab39a7cecc3
yogyy <yogimr261@gmail.com>
2023-10-12 23:18:29 +0700

    feat: add loader in root layout

--- src/components/layouts/layout.tsx
+++ src/components/layouts/layout.tsx
@@ -1,9 +1,11 @@
-import Navbar from './Navbar';
+import Navbar from './navbar';
 import { Poppins } from 'next/font/google';
-import { HeadMetaData } from './HeadMetaTag';
-import Footer from './Footer';
+import { HeadMetaData } from './head-meta';
+import Footer from './footer';
 import { HTMLAttributes } from 'react';
 import { Toaster } from '../UI/toaster';
+import { useSession } from 'next-auth/react';
+import LoaderBlock from '../loader/loaderblock';
 const poppins = Poppins({ weight: '400', subsets: ['latin'] });
 
 interface LayoutProps extends HTMLAttributes<HTMLDivElement> {
@@ -24,6 +26,8 @@ const RootLayout = (props: LayoutProps) => {
     className,
   } = props;
 
+  const { data: session } = useSession();
+
   return (
     <>
       <HeadMetaData
@@ -31,14 +35,18 @@ const RootLayout = (props: LayoutProps) => {
         ogImageUrl={image}
         title={title}
       />
-      <>
-        <header className="sticky top-0 z-20">
-          <Navbar />
-        </header>
-        <main className={(poppins.className, className)}>{children}</main>
-        <Toaster />
-        {footer && <Footer />}
-      </>
+      {!session ? (
+        <LoaderBlock className="bg-gradient-to-b from-ireng to-black" />
+      ) : (
+        <>
+          <header className="sticky top-0 z-20">
+            <Navbar />
+          </header>
+          <main className={(poppins.className, className)}>{children}</main>
+          <Toaster />
+          {footer && <Footer />}
+        </>
+      )}
     </>
   );
 };
