ef86a7e132ac818eeb850697575f6be235d11f15
yogyy <yogimr261@gmail.com>
2023-10-12 18:44:58 +0700

    fix: change image size and not using next image optimized

--- src/components/layouts/thumbnail.tsx
+++ src/components/layouts/thumbnail.tsx
@@ -1,11 +1,12 @@
-import * as React from 'react';
 import Link from 'next/link';
-import Image from 'next/image';
-import { Movie } from '~/typing';
-import { changeModalState, changeMovieState } from '~/atoms/jotaiAtoms';
+import * as React from 'react';
 import { useAtom } from 'jotai';
+import { Movie } from '~/typing';
 import { cn } from '@/lib/utils';
+import NextImage from '../next-image';
+import { imgUrl } from '~/constants/movie';
 import { type VariantProps, cva } from 'class-variance-authority';
+import { changeModalState, changeMovieState } from '~/atoms/jotaiAtoms';
 
 export interface ThumbnailProps<T>
   extends React.HTMLAttributes<T>,
@@ -14,11 +15,11 @@ export interface ThumbnailProps<T>
 }
 
 const thumbnailVariants = cva(
-  'relative min-w-full bg-zinc-900 rounded h-full',
+  'relative min-w-full bg-zinc-800 rounded h-full',
   {
     variants: {
       variant: {
-        portrait: 'aspect-[27/40] w-[92px]',
+        portrait: 'aspect-poster w-[92px]',
         landscape: 'aspect-video w-[96.43px]',
       },
     },
@@ -38,8 +39,8 @@ export function ThumbnailTrailer({
 
   const thumbnailUrl =
     variant === 'portrait'
-      ? `https://image.tmdb.org/t/p/w342/${movie.poster_path}`
-      : `https://image.tmdb.org/t/p/w780/${movie.backdrop_path}`;
+      ? `${imgUrl}/w220_and_h330_bestv2${movie.poster_path}`
+      : `${imgUrl}/w500/${movie.backdrop_path}`;
 
   return (
     <button
@@ -48,15 +49,13 @@ export function ThumbnailTrailer({
         setShowModal(true);
       }}
       title={movie.name || movie.title}
-      className="w-full brightness-90 focus:brightness-110 hover:brightness-110 transition duration-200">
+      type="button"
+      className="w-full rounded">
       <div className={cn(thumbnailVariants({ variant, className }))}>
-        <Image
+        <NextImage
           src={thumbnailUrl}
-          className="object-center rounded-sm md:rounded"
-          fill
-          sizes="100%"
+          className="absolute object-fill rounded-sm md:rounded"
           alt={`thumbnail ${movie?.name}`}
-          draggable={false}
         />
       </div>
     </button>
@@ -70,22 +69,19 @@ export function ThumbnailToPage({
 }: ThumbnailProps<HTMLAnchorElement>) {
   const thumbnailUrl =
     variant === 'portrait'
-      ? `https://image.tmdb.org/t/p/w342/${movie.poster_path}`
-      : `https://image.tmdb.org/t/p/w780/${movie.backdrop_path}`;
+      ? `${imgUrl}/w220_and_h330_bestv2${movie.poster_path}`
+      : `${imgUrl}/w500${movie.backdrop_path}`;
 
   return (
     <Link
       href={`/${movie?.release_date ? 'movie' : 'tv'}/${movie?.id}`}
       title={movie.name || movie.title}
-      className="w-full brightness-90 focus:brightness-110 hover:brightness-110 transition duration-200">
+      className="w-full rounded">
       <div className={cn(thumbnailVariants({ variant, className }))}>
-        <Image
+        <NextImage
           src={thumbnailUrl}
-          className="object-center rounded-sm md:rounded"
-          fill
-          sizes="100%"
+          className="object-fill rounded-sm md:rounded"
           alt={`thumbnail ${movie.name || movie.title}`}
-          draggable={false}
         />
       </div>
     </Link>
