377b7fcf0d605c647674b2b563b618a8a1b92339
yogyy <yogimr261@gmail.com>
2024-01-23 01:34:16 +0700

    chore: fix icon for o-auth

--- src/components/auth/o-auth.tsx
+++ src/components/auth/o-auth.tsx
@@ -1,23 +1,26 @@
-import { signIn } from 'next-auth/react';
-import { Github, Google } from '@/components/icons';
+import { signIn } from "next-auth/react";
+import { Github, Google } from "@/components/icons";
 
 const OAuth: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({
   ...props
 }) => {
   return (
     <div
-      className="flex flex-row items-center justify-center gap-4 mt-4"
-      {...props}>
+      className="mt-4 flex flex-row items-center justify-center gap-4"
+      {...props}
+    >
       <button
         type="button"
-        onClick={() => signIn('google', { callbackUrl: '/profiles' })}
-        className="flex items-center justify-center w-10 h-10 transition bg-transparent rounded-full cursor-pointer hover:brightness-50">
+        onClick={() => signIn("google", { callbackUrl: "/profiles" })}
+        className="flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-transparent transition hover:brightness-50"
+      >
         <Google size="30px" />
       </button>
       <button
         type="button"
-        onClick={() => signIn('github', { callbackUrl: '/profiles' })}
-        className="flex items-center justify-center w-10 h-10 transition bg-transparent rounded-full cursor-pointer hover:brightness-50">
+        onClick={() => signIn("github", { callbackUrl: "/profiles" })}
+        className="flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-transparent transition hover:brightness-50"
+      >
         <Github size="30px" className="stroke-white " />
       </button>
     </div>
