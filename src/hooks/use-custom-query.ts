4a499b3b67de080db4623d536039d271bafa95d5
yogyy <yogimr261@gmail.com>
2024-01-22 23:52:27 +0700

    chore: move fetcher to utils

--- src/hooks/use-custom-query.ts
+++ src/hooks/use-custom-query.ts
@@ -1,17 +1,17 @@
-import fetcher from '../lib/fetcher';
-import { Movie, MovieResponse } from '~/types/tmdb-type';
-import { QueryKey, useQuery, UseQueryOptions } from '@tanstack/react-query';
+import { fetcher } from "../lib/utils";
+import { Movie, MovieResponse } from "~/types/tmdb-type";
+import { QueryKey, useQuery, UseQueryOptions } from "@tanstack/react-query";
 
 export function useCustomQuery(
   queryKey: QueryKey,
   url: string,
-  options?: UseQueryOptions<Movie[], Error>
+  options?: UseQueryOptions<Movie[], Error>,
 ) {
   return useQuery<Movie[], Error>(
     queryKey,
-    async () => await fetcher<MovieResponse>(url).then(res => res.results),
+    async () => await fetcher<MovieResponse>(url).then((res) => res.results),
     {
       ...options,
-    }
+    },
   );
 }
