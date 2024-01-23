2d7315609dd08e30f672becb8ddc650245046abc
yogyy <yogimr261@gmail.com>
2024-01-23 22:23:02 +0700

    chore: remove divs

--- src/pages/anime.tsx
+++ src/pages/anime.tsx
@@ -17,36 +17,33 @@ const AnimePage = () => {
     <RootLayout
       title="Anime"
       description="Stay up-to-date with the latest anime releases. Explore new episodes, series, and exciting titles in the world of Japanese animation. Find release schedules, episode summaries, and streaming options for the hottest anime releases, all in one place"
+      className="mx-auto flex max-w-7xl flex-col gap-4 p-3 pb-5 sm:pt-16 xl:flex-row"
     >
-      <div className="p-3 pb-5">
-        <div className="mx-auto flex max-w-7xl flex-col gap-4 sm:pt-16 xl:flex-row">
-          <section className="flex w-full flex-col gap-4">
-            <div className="flex">
-              <div className="relative mx-auto w-full max-w-[948px] lg:px-0">
-                <SwiperAnime
-                  loading={isLoadingBannerAnime}
-                  bannerAnime={bannerAnime}
-                  aria-label="swiper-anime"
-                />
-              </div>
-            </div>
-            <AnimeAiring aria-label="airing-anime" />
-          </section>
-          <aside className="mx-auto h-max w-full max-w-[948px] rounded bg-[#1C1C1C] p-2 xl:w-[380px]">
-            <h1 className="mb-2 text-xl font-semibold text-primary">
-              More Popular Anime Weekly
-            </h1>
-            {isLoadingBannerAnime ? (
-              <AnimeWeeklyLoading />
-            ) : (
-              <AnimeWeekly
-                bannerAnime={bannerAnime?.slice(10, 20)}
-                aria-label="weekly-anime"
-              />
-            )}
-          </aside>
+      <section className="mt-3 flex w-full flex-col gap-4">
+        <div className="flex">
+          <div className="relative mx-auto w-full max-w-[948px] lg:px-0">
+            <SwiperAnime
+              loading={isLoadingBannerAnime}
+              bannerAnime={bannerAnime}
+              aria-label="swiper-anime"
+            />
+          </div>
         </div>
-      </div>
+        <AnimeAiring aria-label="airing-anime" />
+      </section>
+      <aside className="mx-auto h-max w-full max-w-[948px] rounded bg-[#1C1C1C] p-2 xl:mt-3 xl:w-[380px]">
+        <h1 className="mb-2 text-xl font-semibold text-primary">
+          More Popular Anime Weekly
+        </h1>
+        {isLoadingBannerAnime ? (
+          <AnimeWeeklyLoading />
+        ) : (
+          <AnimeWeekly
+            bannerAnime={bannerAnime?.slice(10, 20)}
+            aria-label="weekly-anime"
+          />
+        )}
+      </aside>
     </RootLayout>
   );
 };
