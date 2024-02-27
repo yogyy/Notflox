9c84e126a057bdaca01d8529e06a7b144c718fbf
yogyy <yogimr261@gmail.com>
2024-02-27 15:08:05 +0700

    feat: add spinner icons

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,3 +1,4 @@
+export { Spinner } from "./spinner";
 export { Netflix } from "./netflix";
 export { Google } from "./google";
 export { Github } from "./github";
---
+++ src/components/icons/spinner.tsx
@@ -0,0 +1,29 @@
+interface Props extends React.SVGProps<SVGSVGElement> {
+  size?: number | string;
+}
+export const Spinner = ({ size = 20, ...props }: Props) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <circle
+        className="opacity-25"
+        cx="12"
+        cy="12"
+        r="10"
+        stroke="currentColor"
+        strokeWidth="4"
+      ></circle>
+      <path
+        className="opacity-75"
+        fill="currentColor"
+        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+      ></path>
+    </svg>
+  );
+};
