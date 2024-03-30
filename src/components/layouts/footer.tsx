098cc044b24d59518e167ec917a307f0371d3be3
yogyy <yogimr261@gmail.com>
2024-03-30 19:32:07 +0700

    feat: add svg github
    add link to github profile  for name

--- src/components/layouts/footer.tsx
+++ src/components/layouts/footer.tsx
@@ -1,8 +1,9 @@
 import Link from "next/link";
 import { Netflix } from "../icons";
 
+const date = new Date().getFullYear();
+
 export const Footer = () => {
-  const date = new Date().getFullYear();
   return (
     <footer className="bg-ireng transition-colors">
       <hr className="border-gray-700 pt-4 sm:mx-auto md:mb-6" />
@@ -12,15 +13,31 @@ export const Footer = () => {
         </div>
         <div className="flex flex-row items-center justify-between gap-6 text-gray-400">
           <span className="flex gap-3 text-sm font-semibold">
-            © {date} yogyy.
+            © {date}{" "}
+            <Link href="https://github.com/yogyy" target="_blank">
+              yogyy .
+            </Link>
           </span>
           <Link
-            className="font-semibold hover:text-gray-600 dark:hover:text-gray-200"
+            className="font-semibold transition-colors duration-200 hover:text-gray-600 dark:hover:text-gray-200"
             href="https://github.com/yogyy/Netflix-Movie-App"
             title="source code"
             target="_blank"
           >
-            Github
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
+              <path d="M9 18c-4.51 2-5-2-7-2" />
+            </svg>
           </Link>
         </div>
       </div>
