5f2dd9ce2d132d4e50715249af971cb21d40dd9d
yogyy <yogimr261@gmail.com>
2025-04-27 20:55:26 +0700

    chore: update auth schema, add new atom

--- atoms/auth-atoms.ts
+++ atoms/auth-atoms.ts
@@ -1,6 +1,7 @@
 import { atom } from "jotai";
 
 export const loginState = atom<boolean>(true);
+export const providerState = atom<"google" | "github" | null>(null);
 
 export const changeLoginState = atom(null, (get, set, newState: boolean) => {
   set(loginState, newState);
--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -2,10 +2,10 @@ import { z } from "zod";
 
 export const LoginSchema = z.object({
   email: z.string().email({
-    message: "Email is required",
+    message: "Please enter a valid email",
   }),
   password: z.string().min(1, {
-    message: "Password is required",
+    message: "Your password must contain between 4 and 60 characters.",
   }),
   username: z.optional(z.string()),
   code: z.optional(z.string()),
