8f0284f1ae9d188ccf9d2af89d1696d35285b82e
yogyy <yogimr261@gmail.com>
2025-05-02 13:18:56 +0700

    fix color

--- src/components/navbar-item.tsx
+++ src/components/navbar-item.tsx
@@ -3,32 +3,33 @@ import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-interface NavbarItemProps extends React.ComponentProps<"div"> {}
+interface NavbarItemProps extends React.ComponentProps<"ul"> {}
 export const NavbarItem = ({ className, ...props }: NavbarItemProps) => {
   const router = useRouter();
   const arrOfRoute = router.route.split("/");
   const baseRoute = "/" + arrOfRoute[1];
 
   return (
-    <div className={cn("font-semibold", className)} {...props}>
-      <ul className="flex flex-col items-start gap-3 sm:flex-row">
-        {links.map((link) => (
-          <li key={link.name}>
-            <Link
-              href={link.url}
-              className={cn(
-                "rounded px-1 font-bold transition-colors hover:text-primary/80 focus:outline-none focus-visible:ring-2",
-                router.pathname && baseRoute !== link.url
-                  ? "text-white focus-visible:ring-primary"
-                  : "text-primary focus-visible:ring-white",
-              )}
-            >
-              {link.name}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <ul
+      className={cn("flex flex-col items-start gap-3 sm:flex-row", className)}
+      {...props}
+    >
+      {links.map((link) => (
+        <li key={link.name} className="tracking-wider">
+          <Link
+            href={link.url}
+            className={cn(
+              "rounded px-1 font-normal transition-colors hover:text-zinc-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-zinc-100",
+              router.pathname && baseRoute !== link.url
+                ? "text-zinc-100/80"
+                : "font-semibold text-zinc-50 hover:text-current",
+            )}
+          >
+            {link.name}
+          </Link>
+        </li>
+      ))}
+    </ul>
   );
 };
 
