957da145c384b62d5b513a312f3e632246b0acdd
yogyy <yogimr261@gmail.com>
2025-05-02 13:17:25 +0700

    fix: fix navbar on mobile, remove sheet

--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -3,17 +3,8 @@ import { Netflix } from "../icons";
 import { NavbarItem } from "../navbar-item";
 import { Notifications } from "../notification";
 import Link from "next/link";
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
 import { cn } from "@/lib/utils";
 import { SearchInput } from "../search-input";
-import { BurgerMenu } from "../icons/general";
 import { useEffect, useState } from "react";
 
 export const Header = () => {
@@ -43,29 +34,12 @@ export const Header = () => {
       >
         <div
           className={cn(
-            "mx-auto flex h-16 w-full min-w-fit max-w-7xl flex-row items-center justify-between px-5",
+            "mx-auto flex h-16 w-full min-w-fit max-w-7xl flex-row items-center justify-between sm:px-5",
           )}
         >
           <div className="flex items-center justify-center">
-            <Sheet>
-              <SheetTrigger className="block rounded-md px-2 py-1 hover:bg-primary/10 sm:hidden">
-                <BurgerMenu />
-                <span className="sr-only">open drawer</span>
-              </SheetTrigger>
-              <SheetContent
-                side="top"
-                className="w-full bg-gradient-to-r from-black to-ireng"
-              >
-                <SheetHeader>
-                  <SheetTitle className="mb-4">
-                    <Netflix className="h-4" />
-                  </SheetTitle>
-                  <SheetDescription asChild>
-                    <NavbarItem className="flex flex-col items-start gap-3" />
-                  </SheetDescription>
-                </SheetHeader>
-              </SheetContent>
-            </Sheet>
+            <NavbarItem className="flex scale-90 flex-row sm:hidden" />
+
             <Link
               href="/"
               className="flex items-center justify-center focus:py-1 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-600"
