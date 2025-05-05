05655deb06f1dc57becee83f31901c32e9cf5526
yogyy <yogimr261@gmail.com>
2025-05-05 17:37:05 +0700

    chore: move location client schema

--- src/lib/client/schema.ts
+++ src/lib/client/schema.ts
@@ -7,6 +7,7 @@ export const LoginSchema = z.object({
   password: z.string().min(1, {
     message: "Your password must contain between 4 and 60 characters.",
   }),
-  username: z.optional(z.string()),
-  code: z.optional(z.string()),
+  username: z.string().min(7, {
+    message: "username must contain between 7 and 20 characters.",
+  }),
 });
