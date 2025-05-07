5ee3b77c5c9223377240f337e052f1f02a70793b
yogyy <yogimr261@gmail.com>
2025-05-07 20:03:53 +0700

    fix: fixing airing anime pagination

--- src/components/layouts/anime/airing.tsx
+++ src/components/layouts/anime/airing.tsx
@@ -1,39 +1,52 @@
 import Link from "next/link";
-import { useState } from "react";
-import { Paginate } from "../paginate";
+import { useEffect, useState } from "react";
 import { ImageNotFound, NextImage } from "@/components/next-image";
 import { MovieResponse } from "~/types/tmdb-type";
-import { useQueries } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { baseUrl, imgUrl } from "~/constants/movie";
 import { AiringAnimeSkeleton } from "@/components/loader/skeleton";
 import { cn, fetcher } from "@/lib/utils";
 import { today } from "@/lib/get-date";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
 
 interface AiringProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+const fetchAiringAnime = async (page: number): Promise<MovieResponse> => {
+  return fetcher(
+    `${baseUrl}/discover/tv?page=${page}&sort_by=first_air_date.desc&first_air_date.lte=${today}&with_keywords=210024&with_original_language=ja|zh`,
+  );
+};
+
 export const AiringAnime = ({ className, ...props }: AiringProps) => {
-  const queryConfigurations = Array.from({ length: 4 }, (_, page) => ({
-    queryKey: ["anime-airing", page + 1],
-    queryFn: () => {
-      return fetcher<MovieResponse>(
-        `${baseUrl}/discover/tv?page=${
-          page + 1
-        }&sort_by=first_air_date.desc&first_air_date.lte=${today}&with_keywords=210024&with_original_language=ja|zh`,
-      );
-    },
-  }));
-  const queryResults = useQueries({ queries: queryConfigurations });
-  const combinedData = queryResults.map((res) => res.data?.results).flat();
-  const isLoading = queryResults.some((res) => res.isLoading);
-  const [currentPage, setCurrentPage] = useState(1);
-  const postPerPage = 12;
-  const lastPostIndex = currentPage * postPerPage;
-  const firstPostIndex = lastPostIndex - postPerPage;
-  const currentAiring = combinedData?.slice(firstPostIndex, lastPostIndex);
+  const queryClient = useQueryClient();
+  const [page, setPage] = useState(1);
+
+  const { data, isLoading, isPlaceholderData } = useQuery({
+    queryKey: ["anime-airing", page],
+    queryFn: () => fetchAiringAnime(page),
+    keepPreviousData: true,
+  });
+
+  useEffect(() => {
+    if (!isPlaceholderData && data?.page! < data?.total_pages!) {
+      queryClient.prefetchQuery({
+        queryKey: ["anime-airing", page],
+        queryFn: () => fetchAiringAnime(page),
+      });
+    }
+  }, [data, isPlaceholderData, page, queryClient]);
 
   return (
     <div className={cn("mx-auto w-full max-w-[948px]", className)} {...props}>
       <div className="w-full py-1">
-        <span id="recent-released" className="absolute -mt-16 md:-mt-20" />
+        <span id="asrecent-released" className="absolute -mt-16 md:-mt-20" />
         <h1 className="mb-5 text-xl font-semibold tracking-wide text-primary">
           Recent Released
         </h1>
@@ -42,7 +55,7 @@ export const AiringAnime = ({ className, ...props }: AiringProps) => {
         <AiringAnimeSkeleton />
       ) : (
         <ul className="moviecard-container relative grid grid-cols-2 place-content-center gap-3 min-[320px]:grid-cols-3 sm:grid-cols-4 md:grid-cols-5 xl:grid-cols-6">
-          {currentAiring?.map((tv) => (
+          {data?.results.map((tv) => (
             <li
               key={tv?.id}
               className="moviecard group relative overflow-hidden"
@@ -64,7 +77,7 @@ export const AiringAnime = ({ className, ...props }: AiringProps) => {
                   )}
                 </div>
                 <span className="max-h-8 w-full text-pretty text-[#ababab] transition duration-200 group-focus-within:text-white group-hover:text-white">
-                  <p className="flex justify-center truncate text-balance text-center text-[14px] leading-4">
+                  <p className="truncate text-center text-[14px] leading-4">
                     {tv?.name}
                   </p>
                 </span>
@@ -73,16 +86,58 @@ export const AiringAnime = ({ className, ...props }: AiringProps) => {
           ))}
         </ul>
       )}
-      {!isLoading && (
-        <Paginate
-          id_href="#recent-released"
-          className="mt-5 pt-1"
-          currentPage={currentPage}
-          postPerPage={postPerPage}
-          setCurrentPage={setCurrentPage}
-          totalPost={combinedData.length}
-        />
-      )}
+      <Pagination
+        className={cn(
+          "mt-2 w-full scale-90 overflow-hidden overflow-x-scroll pb-2 pt-1 scrollbar-hide md:scale-100",
+          className,
+        )}
+        {...props}
+      >
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious
+              href="#recent-released"
+              className={
+                page > 1
+                  ? "focus-visible:ring-primary"
+                  : "pointer-events-none opacity-50 focus-visible:ring-0"
+              }
+              onClick={() => {
+                if (page > 1) {
+                  setPage((prev) => prev - 1);
+                }
+              }}
+            />
+          </PaginationItem>
+          {[1, 2, 3, 4].map((curr) => (
+            <PaginationItem key={curr} className="">
+              <PaginationLink
+                href="#recent-released"
+                isActive={curr === page}
+                onClick={() => setPage(curr)}
+                className="focus-visible:ring-primary"
+              >
+                {curr}
+              </PaginationLink>
+            </PaginationItem>
+          ))}
+          <PaginationItem>
+            <PaginationNext
+              className={
+                page < 4
+                  ? "focus-visible:ring-primary"
+                  : "pointer-events-none opacity-50 focus-visible:ring-0"
+              }
+              href="#recent-released"
+              onClick={() => {
+                if (page < 4) {
+                  setPage((prev) => prev + 1);
+                }
+              }}
+            />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
     </div>
   );
 };
