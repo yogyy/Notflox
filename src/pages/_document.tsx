f972b10920f1e21c2243a9b73a6a2eb384bfc3a7
yogyy <yogimr261@gmail.com>
2025-05-11 00:35:40 +0700

    chore: update script tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,25 +1,23 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {process.env.NODE_ENV === "production" ? (
-          <script
-            defer
-            src="/script.analytics.js"
-            data-website-id={process.env.NEXT_PUBLIC_UMAMI_ID}
-          />
-        ) : (
-          <script
-            async
-            src="https://unpkg.com/react-scan/dist/auto.global.js"
-          />
+        {process.env.NODE_ENV === "development" && (
+          <script async src="https://unpkg.com/react-scan/dist/auto.global.js" />
         )}
       </Head>
       <body>
         <Main />
         <NextScript />
+        <Script
+          defer
+          src="/script.analytics.js"
+          strategy="beforeInteractive"
+          data-website-id={process.env.NEXT_PUBLIC_UMAMI_ID}
+        />
       </body>
     </Html>
   );
